bfcac2956ab7e80bfacbee4f76eca02319c87453
Евгений Афанасьев <evgenij29082002@gmail.com>
2021-11-09 17:50:38 +0800

    создал класс температуры, добавил в него функции вывода, сложения, вычитания и умножения

---
+++ TemperatureOver39/Temperature.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureOver39
+{
+    public enum TempType { c, f, r, k }
+    public class Temperature
+    {
+        private double value;
+        private TempType type;
+        public Temperature(double value, TempType type)
+        {
+            this.value = value;
+            this.type = type;
+        }
+        public string Output()
+        {
+            string typeOut = "";
+            switch (this.type)
+            {
+                case TempType.c:
+                    typeOut = "°C";
+                    break;
+                case TempType.f:
+                    typeOut = "°F";
+                    break;
+                case TempType.r:
+                    typeOut = "°Ra";
+                    break;
+                case TempType.k:
+                    typeOut = "K";
+                    break;
+            }
+            return String.Format("{0} {1}", this.value, typeOut);
+        }
+        public static Temperature operator+(Temperature instance, double number)
+        {
+            var newValue = instance.value + number;
+            var temp = new Temperature(newValue, instance.type);
+            return temp;
+        }
+        public static Temperature operator+(double number, Temperature instance)
+        {
+            return instance + number;
+        }
+        public static Temperature operator -(Temperature instance, double number)
+        {
+            var newValue = instance.value - number;
+            var temp = new Temperature(newValue, instance.type);
+            return temp;
+        }
+        public static Temperature operator -(double number, Temperature instance)
+        {
+            var newValue = number - instance.value;
+            var temp = new Temperature(newValue, instance.type);
+            return temp;
+        }
+        public static Temperature operator *(Temperature instance, double number)
+        {
+            var newValue = instance.value * number;
+            var temp = new Temperature(newValue, instance.type);
+            return temp;
+        }
+        public static Temperature operator *(double number, Temperature instance)
+        {
+            return instance * number;
+        }
+    }
+}
---
+++ TemperatureOver39Tests/TemperatureTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TemperatureOver39;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureOver39.Tests
+{
+    [TestClass()]
+    public class TemperatureTests
+    {
+        [TestMethod()]
+        public void OutputTest()
+        {
+            var temp = new Temperature(10, TempType.c);
+            Assert.AreEqual("10 °C", temp.Output());
+
+            temp = new Temperature(10, TempType.f);
+            Assert.AreEqual("10 °F", temp.Output());
+
+            temp = new Temperature(10, TempType.r);
+            Assert.AreEqual("10 °Ra", temp.Output());
+
+            temp = new Temperature(10, TempType.k);
+            Assert.AreEqual("10 K", temp.Output());
+        }
+        [TestMethod()]
+        public void AddNumberTest()
+        {
+            var temp = new Temperature(36.6, TempType.c);
+            temp = temp + 3.25;
+            Assert.AreEqual("39,85 °C", temp.Output());
+        }
+        [TestMethod()]
+        public void SubNumberTest()
+        {
+            var temp = new Temperature(40.2, TempType.c);
+            temp = temp - 3.25;
+            Assert.AreEqual("36,95 °C", temp.Output());
+        }
+        [TestMethod()]
+        public void MulByNumberTest()
+        {
+            var temp = new Temperature(41, TempType.f);
+            temp = temp * 3;
+            Assert.AreEqual("123 °F", temp.Output());
+        }
+    }
+}
